0d1990532a9c48ae7a80ed6b8cc935caf382e364
Andry <akula996@gmail.com>
2021-02-08 00:53:04 +0300

    first version

---
+++ Assets/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public GameObject gameOverPanel;
+    public GameObject winPanel;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        audioSource.Stop();
+        Destroy(player);
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Win()
+    {
+        audioSource.Stop();
+        winPanel.SetActive(true);
+    }
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour
+{
+
+    public int count;
+    public Type type;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.tag = type.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public enum Type
+    {
+        GUN,
+        BOMB,
+        KEY
+
+    }
+}
